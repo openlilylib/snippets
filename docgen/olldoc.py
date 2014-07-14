6ff244478c9729f5effd3a06ccad12824e65180b
Urs Liska <git@ursliska.de>
2014-07-14 10:55:43 +0200

    oll-doc: Add MetadataWidget()
    A widget (may later be moved to a dialog or dockable panel)
    to display (and later edit) snippet metadata.
    
    This commit simply sets up the widget and places it in the
    main window. On loading of a snippet it displays the content
    of the snippet's metadata in line or text edits.

--- docgen/olldoc.py
+++ docgen/olldoc.py
@@ -4,6 +4,7 @@
 import sys, os
 from PyQt4 import QtCore,  QtGui
 import snippets
+import metadata
 
 class AppInfo(QtCore.QObject):
     """Stores global information about the application
@@ -32,51 +33,69 @@ class MainWindow(QtGui.QMainWindow):
         # TEMPORARY
         self.temporaryFileDump()
         
-        
-    
     def createComponents(self):
-        self.labelOverview = QtGui.QLabel("Elements in " + appInfo.defPath + ":")
+        self.labelOverview = QtGui.QLabel("Library directory: " + appInfo.defPath)
+
+        # Browsing Tree View
         self.labelBrowse = QtGui.QLabel("Browse Snippets:")
+        self.tvBrowse = QtGui.QTreeView()
+        self.modelBrowse = QtGui.QStandardItemModel()
+        self.modelBrowse.setHorizontalHeaderLabels(['Browse snippets'])
+        self.tvBrowse.setModel(self.modelBrowse)
+        self.tvBrowse.setUniformRowHeights(True)
+        self.tvBrowse.header().hide()
+
+        # Snippet Definition
         self.labelDefinition = QtGui.QLabel("Snippet Definition:")
-        self.labelExample = QtGui.QLabel("Usage Example:")
-        
+        self.metadataWidget = metadata.MetadataWidget(self)
         self.teDefinition = QtGui.QTextEdit()
         self.teDefinition.setReadOnly(True)
+
+        # Snippet's Usage Example
+        self.labelExample = QtGui.QLabel("Usage Example:")
         self.teExample = QtGui.QTextEdit()
         self.teExample.setReadOnly(True)
-        
-        self.tvNavigate = QtGui.QTreeView()
-        self.modelNavigate = QtGui.QStandardItemModel()
-        self.modelNavigate.setHorizontalHeaderLabels(['Browse snippets'])
-        self.tvNavigate.setModel(self.modelNavigate)
-        self.tvNavigate.setUniformRowHeights(True)
-        self.tvNavigate.header().hide()
 
+        # Buttons
         self.pbReread = QtGui.QPushButton("Read again")
         self.pbExit = QtGui.QPushButton("Exit")
-        
+    
     def createConnects(self):
         self.pbReread.clicked.connect(self.readSnippets)
         self.pbExit.clicked.connect(self.close)
         
-        self.tvNavigate.clicked.connect(self.snippetRowClicked)
+        self.tvBrowse.clicked.connect(self.snippetRowClicked)
 
     def createLayout(self):
         centralWidget = QtGui.QWidget()
         centralLayout = cl = QtGui.QGridLayout()
+        snDefinitionLayout = sl = QtGui.QVBoxLayout()
+        snMetadataLayout = sm = QtGui.QGridLayout()
+        
+        
+        cl.addLayout(sl, 3, 1)
+        #sl.addLayout(sm)
+        sl.addWidget(self.metadataWidget)
+        sl.addWidget(self.teDefinition)
 
         cl.addWidget(self.labelOverview, 0, 0, 1, 3)
         cl.addWidget(self.labelBrowse, 2, 0)
-        cl.addWidget(self.tvNavigate, 3, 0)
+        cl.addWidget(self.tvBrowse, 3, 0)
         cl.addWidget(self.labelDefinition, 2, 1)
-        cl.addWidget(self.teDefinition, 3, 1)
+#        cl.addWidget(self.teDefinition, 3, 1)
         cl.addWidget(self.labelExample, 2, 2)
         cl.addWidget(self.teExample, 3, 2)
         cl.addWidget(self.pbReread, 5, 0)
         cl.addWidget(self.pbExit, 5, 2)
         centralWidget.setLayout(centralLayout)
-        self.setCentralWidget(centralWidget)#
+        self.setCentralWidget(centralWidget)
     
+    def displaySnippetMetadata(self, snippet):
+        dfn = snippet.definition
+        self.leIncludeName.setText(snippet.name)
+        self.leSnippetTitle.setText(dfn.headerFields['snippet-title'])
+        self.leShortDesc.setText(' '.join(dfn.headerFields['snippet-short-description']))
+        
     def readSnippets(self):
         # create, read and parse snippets
         if not self.snippets:
@@ -86,13 +105,13 @@ class MainWindow(QtGui.QMainWindow):
         self.displayTree()
     
     def displayTree(self):
-        self.modelNavigate.clear()
+        self.modelBrowse.clear()
 
         numsnippets = ' (' + str(len(self.snippets.snippets)) + ')'
         byName = QtGui.QStandardItem('By Name' + numsnippets)
         for sn in self.snippets.names:
             byName.appendRow(QtGui.QStandardItem(sn))
-        self.modelNavigate.appendRow(byName)
+        self.modelBrowse.appendRow(byName)
 
         byCategory = QtGui.QStandardItem('By Category')
         for c in self.snippets.categories['names']:
@@ -101,7 +120,7 @@ class MainWindow(QtGui.QMainWindow):
             byCategory.appendRow(cat)
             for s in self.snippets.categories[c]:
                 cat.appendRow(QtGui.QStandardItem(s))
-        self.modelNavigate.appendRow(byCategory)
+        self.modelBrowse.appendRow(byCategory)
         
         byTag = QtGui.QStandardItem('By Tag')
         for t in self.snippets.tags['names']:
@@ -110,7 +129,7 @@ class MainWindow(QtGui.QMainWindow):
             byTag.appendRow(tag)
             for s in self.snippets.tags[t]:
                 tag.appendRow(QtGui.QStandardItem(s))
-        self.modelNavigate.appendRow(byTag)
+        self.modelBrowse.appendRow(byTag)
         
         byAuthor = QtGui.QStandardItem('By Author')
         for a in self.snippets.authors['names']:
@@ -119,13 +138,14 @@ class MainWindow(QtGui.QMainWindow):
             byAuthor.appendRow(author)
             for s in self.snippets.authors[a]:
                 author.appendRow(QtGui.QStandardItem(s))
-        self.modelNavigate.appendRow(byAuthor)
+        self.modelBrowse.appendRow(byAuthor)
 
     def snippetRowClicked(self, index):
-        name = unicode(self.modelNavigate.itemFromIndex(index).text())
+        name = unicode(self.modelBrowse.itemFromIndex(index).text())
         snippet = self.snippets.byName(name)
         if snippet is not None:
-            self.teDefinition.setText(''.join(snippet.definition.filecontent))
+            self.metadataWidget.showSnippet(snippet)
+            self.teDefinition.setText(''.join(snippet.definition.bodycontent))
             self.teExample.setText(''.join(snippet.example.filecontent))
 
     def temporaryFileDump(self):
