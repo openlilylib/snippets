d7e2e4a7ddd880a392eed6b8c5723d176a310715
Urs Liska <git@ursliska.de>
2015-02-21 22:28:38 +0100

    test: improve version string regex
    It was necessary to change at least the last part of the match
    because the third part of the version string can well be one
    or two digits long.
    
    The commit modifies all three string segments to be at least
    one digit long because in theory also the major and minor
    versions might be of any length.
    
    Conflicts:
    	test/simple_tests.py

--- test/simple_tests.py
+++ test/simple_tests.py
@@ -91,13 +91,13 @@ class SimpleTests:
     def __lilypond_version(self):
         lily = sp.Popen([self.lily_command, "-v"], stdout=sp.PIPE, stderr=sp.PIPE)
         version_line = lily.communicate()[0].splitlines()[0]
-        return re.search(r"\d\.\d\d\.\d", version_line).group(0)
+        return re.search(r"\d+\.\d+\.\d+", version_line).group(0)
 
     def is_runnable_file(self, fname):
         """Returns true if fname can be compiled with the lilypond version used"""
         with open(fname, 'r') as fcontents:
             for line in fcontents.readlines():
-                version_line = re.search(r"\\version \"(\d\.\d\d\.\d)\"", line)
+                version_line = re.search(r"\\version \"(\d+\.\d+\.\d+)\"", line)
                 if version_line:
                     file_version = version_line.group(1).split(".")
                     lily_version = self.lilypond_version.split(".")
